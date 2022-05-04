2494951c5c2a3ccf7c909dea15c1da3b5136c505
Okonu <ianyakundi015@gmail.com>
2022-05-04 15:39:43 +0300

    commit

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+/**
+ * m - a
+ */
+m{p(); }
---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+
+
+return (1);
+}
+
